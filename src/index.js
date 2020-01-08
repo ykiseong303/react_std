ed1a66683084bb0e425f0c377e6cac7c5908027b
ykiseong303 <41833412+ykiseong303@users.noreply.github.com>
2020-01-08 23:06:25 +0900

    third version
    Game algorithm applied

--- src/index.js
+++ src/index.js
@@ -2,49 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-        
-        // 생성자를 설정하는 것으로 state를 저장
-        // square의 현재 값을 this.state에 저장
-        // Square는 게임의 상태를 유지할 필요가 없기 때문에 생성자를 지움
-        // constructor(props){
-        //     // 클래스의 생성자를 정의할 때 항상 super를 호출 
-        //     super(props);
-        //     this.state = {
-        //         value: null,
-        //     };
-            
-        // }
-        // board로부터 전달받은 prop을 화면에 내보냄
-        // render함수는 화면에서 보고자 하는 내용을 반환함
-        render() {
-        return (
-            <button className="square" 
-            // 해당 컴포넌트 state의 변경 사항을 대기열에 넣고 
-            // react에게 해당 컴포넌트와 갱신된 state를 사용하여 다시 렌더링 되어야한다고 전달
-                onClick={() => this.props.onClick()}>
-            {this.props.value}
-            </button>
-        );
-        // board에서 renderSquare를 통해 각각의 Square 컴포넌트가 만들어졌고
-        // 현재 게임의 state를 각각의 Square 컴포넌트에서 유지하고 있다 
-        // Board가 각 Square에 state를 요청하는 것이 아닌
-        // Board 컴포넌트에 게임의 상태를 저장하는 것이 가장 좋은 방법
-        // 각 Square에게 prop을 전달하는 것으로 무엇을 표시할 지 알려준다
-        }
+// 1. 기존 Square 컴포넌트
+
+// class Square extends React.Component {        
+//         // }
+//         // board로부터 전달받은 prop을 화면에 내보냄
+//         // render함수는 화면에서 보고자 하는 내용을 반환함
+//         render() {
+//         return (
+//             <button className="square" 
+//             // 해당 컴포넌트 state의 변경 사항을 대기열에 넣고 
+//             // react에게 해당 컴포넌트와 갱신된 state를 사용하여 다시 렌더링 되어야한다고 전달
+//                 onClick={() => this.props.onClick()}>
+//             {this.props.value}
+//             </button>
+//         );
+//         }
+// }
+
+// 2. Square컴포넌트를 함수컴포넌트로 변경 
+function Square(props) {
+    // 클래스가 아니므로 this 키워드 삭제 
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
-  
 class Board extends React.Component {
     constructor(props){
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            // 플레이어가 수를 둘 때마다 xIsNext(Boolean)이 뒤집혀 다음 플레이어 결정 후
+            // 게임의 state 저장 
+            xIsNext: true,
         };
     }
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        // 누군가 승리하거나 Square가 이미 채워졌다면 클릭을 무시하도록 지정
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        // xIsNext가 true면 X, false면 O
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        // react에게 렌더링 재실행해야함을 알림
+        this.setState({
+            // i번째 현재 squares의 값을 저장
+            // 현재 xIsNext의 값을 반전해서 저장
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        })
     }
     renderSquare(i) {
         // square에 value prop을 전달
@@ -59,7 +68,14 @@ class Board extends React.Component {
     }
     //화면출력
     render() {
-      const status = 'Next player: X';
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+          status = 'Winner : ' + winner;
+      } else {
+          status = 'Next player : ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+
   
       return (
         <div>
@@ -106,4 +122,22 @@ class Game extends React.Component {
     <Game />,
     document.getElementById('root')
   );
-  
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
